3439c597d2c5722331785f5f0092059726d8f351
Najmus Saquib <145494658+najmus786@users.noreply.github.com>
2025-08-13 21:06:10 +0530

    Time: 9 ms (5.19%) | Memory: 45.6 MB (21.08%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        if(nums.length==1){
+            if(nums[0]>=1){
+                return 0;
+            }
+        }
+        Arrays.sort(nums);
+        if(nums[0]!=0){
+            return 0;
+        }
+        for(int a:nums){
+            hm.put(a,1);
+        }
+        for(int a:nums){
+            if(hm.containsKey(a+1)){
+                continue;
+            }else{
+                return a+1;
+            }
+        }
+        return nums.length;
+    }
+}
