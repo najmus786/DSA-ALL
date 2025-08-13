3fc513410a5e6debcd8631bd3921ebd6d88e0d71
Najmus Saquib <145494658+najmus786@users.noreply.github.com>
2025-08-13 11:13:29 +0530

    Time: 2 ms (98.95%) | Memory: 45 MB (47.01%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,34 +1,13 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int aux[]=Arrays.copyOf(numbers,numbers.length);
-        Arrays.sort(numbers);
-        int i=0;
-        int j=numbers.length-1;
-        int n1=0,n2=0;
-        int ans[]=new int [2];
-        while(i<j){
-            if((numbers[i]+numbers[j])==target){
-                n1=numbers[i];
-                n2=numbers[j];
-                break;
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int rem=target-nums[i];
+            if(hm.containsKey(rem)){
+                return new int[]{i,hm.get(rem)};
             }
-            else if((numbers[i]+numbers[j])>target){
-                j--;
-            }
-            else if(numbers[i]+numbers[j]<target){
-                i++;
-            }
+            hm.put(nums[i],i);
         }
-        int m=-1;
-        int n=-1;
-        for(int k=0;k<aux.length;k++){
-            if(m==-1 && aux[k]==n1){
-                m=k;
-            }
-            else if(n==-1 && aux[k]==n2){
-                n=k;
-            }
-        }
-        return new int []{m,n};
+    return null;
     }
 }
