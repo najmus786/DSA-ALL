671febb4ca4a0a482a6486be4f48a8719cbf69e4
Najmus Saquib <145494658+najmus786@users.noreply.github.com>
2025-08-14 21:33:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (9.45%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] plusOne(int[] arr) {
-        int n=arr.length;
-        for(int i=n-1;i>=0;i--){
+       for(int i=arr.length-1;i>=0;i--){
             if(arr[i]==9){
-                arr[i]=0; 
+                arr[i]=0;
+                continue;
             }else{
-                arr[i]++;
+                arr[i]=arr[i]+1;
                 return arr;
             }
-        }
-        int ans[]=new int [n+1];
-        ans[0]=1;
-        return ans;
+       }
+       if(arr[0]==0){
+            int ans[]=new int[arr.length+1];
+            ans[0]=1;
+            return ans;
+       }
+       return arr;
     }
 }
