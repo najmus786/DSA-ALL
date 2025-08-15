a4713f1bbd21a276ae354f2e24b79e5246413f3e
Najmus Saquib <145494658+najmus786@users.noreply.github.com>
2025-08-15 11:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (10.00%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,21 +1,23 @@
 class Solution {
+    public void swap(int [] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
     public void sortColors(int[] nums) {
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        for(int a:nums){
-            hs.put(a,hs.getOrDefault(a,0)+1);
-        }
-        for(int i=0;i<nums.length;i++){
-            if(hs.containsKey(0) && hs.get(0)!=0){
-                nums[i]=0;
-                hs.put(0,hs.get(0)-1);
-            }
-            else if(hs.containsKey(1)&& hs.get(1)!=0){
-                nums[i]=1;
-                hs.put(1,hs.get(1)-1);
-            }
-            else if(hs.containsKey(2)&& hs.get(2)!=0){
-                nums[i]=2;
-                hs.put(2,hs.get(2)-1);
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==1){
+                mid++;
+            }else if(nums[mid]==0){
+                swap(nums,mid,low);
+                low++;
+                mid++;
+            }else{
+                swap(nums,mid,high);
+                high--;
             }
         }
     }
