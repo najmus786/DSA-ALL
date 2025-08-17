f246e20282b4a6098798b4b3949c56284cfc57d5
Najmus Saquib <145494658+najmus786@users.noreply.github.com>
2025-08-17 09:54:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (5.77%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,9 +3,6 @@ class Solution {
         int i=0;
         int j=nums.length-1;
         int mid=0;
-        if(target<nums[0]){
-            return 0;
-        }
         while(i<=j){
             mid=i+(j-i)/2;
             if(nums[mid]==target){
