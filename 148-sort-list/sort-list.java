47b9a6862a484284a806518e704be3f1e62b091a
Najmus Saquib <145494658+najmus786@users.noreply.github.com>
2025-09-09 22:26:01 +0530

    Time: 11 ms (39.26%) | Memory: 57 MB (38.34%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,18 +9,50 @@
  * }
  */
 class Solution {
+
+    public ListNode mid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
     public ListNode sortList(ListNode head) {
-        ListNode temp=new ListNode(0);
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        while(head!=null){
-            pq.offer(head.val);
-            head=head.next;
+        if(head==null || head.next==null){
+            return head;
         }
-        ListNode kmp=temp;
-        while(!pq.isEmpty()){
-            kmp.next=new ListNode(pq.poll()); 
-            kmp=kmp.next;
+        ListNode mid=mid(head);
+        ListNode orgMid=mid.next;
+        mid.next=null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(orgMid);
+        return mergeTwoLists(left, right);
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode kmp=new ListNode(0);
+        ListNode temp=kmp;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                temp.next=list1;
+                list1=list1.next;
+            }else{
+                temp.next=list2;
+                list2=list2.next;
+            }
+            temp=temp.next;
         }
-        return temp.next;
+        while(list1!=null){
+            temp.next=list1;
+            list1=list1.next;
+            temp=temp.next;
+        }
+        while(list2!=null){
+            temp.next=list2;
+            list2=list2.next;
+            temp=temp.next;
+        }
+        return kmp.next;
     }
 }
